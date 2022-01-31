7010c020ed2db269d980a155c025d148d087a9af
devgony <devgony@gmail.com>
2022-01-31 20:41:24 +0900

    9.1 PoW Proof Of Concept

--- README.md
+++ README.md
@@ -881,3 +881,22 @@ func Checkpoint() []byte {
 ```sh
 rm blockchain.db
 ```
+
+## 9.1 PoW Proof Of Concept (10:13)
+
+- Mining prototype
+
+```go
+difficulty := 2
+target := strings.Repeat("0", difficulty)
+nonce := 1
+for {
+	hash := fmt.Sprintf("%x", sha256.Sum256([]byte("hello"+fmt.Sprint(nonce))))
+	fmt.Printf("Hash:%s\nTarget:%s\nNonce:%d\n\n", hash, target, nonce)
+	if strings.HasPrefix(hash, target) {
+		return
+	} else {
+		nonce++
+	}
+}
+```
--- main.go
+++ main.go
@@ -1,16 +1,25 @@
 package main
 
 import (
-	"github.com/devgony/nomadcoin/blockchain"
-	"github.com/devgony/nomadcoin/cli"
-	"github.com/devgony/nomadcoin/db"
+	"crypto/sha256"
+	"fmt"
+	"strings"
 )
 
 func main() {
-	defer db.Close()
-	blockchain.Blockchain()
-	cli.Start()
-	// blockchain.Blockchain().AddBlock("First")
-	// blockchain.Blockchain().AddBlock("Second")
-	// blockchain.Blockchain().AddBlock("Third")
+	// defer db.Close()
+	// blockchain.Blockchain()
+	// cli.Start()
+	difficulty := 2
+	target := strings.Repeat("0", difficulty)
+	nonce := 1
+	for {
+		hash := fmt.Sprintf("%x", sha256.Sum256([]byte("hello"+fmt.Sprint(nonce))))
+		fmt.Printf("Hash:%s\nTarget:%s\nNonce:%d\n\n", hash, target, nonce)
+		if strings.HasPrefix(hash, target) {
+			return
+		} else {
+			nonce++
+		}
+	}
 }
